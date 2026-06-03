ad2410bf229e2587140c34e971fb7d9af2d2f148
sureshanand1619 <suresh.anand1619@outlook.com>
2026-06-03 08:29:52 +0000

    fix: add types to all variables for staging and prod

--- ha-setup/env/prod/variables.tf
+++ ha-setup/env/prod/variables.tf
@@ -1,35 +1,69 @@
-variable "environment" {}
-variable "location" {}
-variable "resource_group_name" {}
+variable "environment" {
+  type        = string
+  description = "Deployment environment (dev, staging, prod)"
+}
 
-variable "vnet_cidr" {}
-variable "public_subnet_cidr" {}
-variable "private_subnet_cidr" {}
+variable "location" {
+  type        = string
+  description = "Azure region for resource deployment"
+}
 
-variable "allowed_ssh_ip" {}
+variable "resource_group_name" {
+  type        = string
+  description = "Name of the Azure resource group"
+}
 
-variable "admin_username" {}
+variable "vnet_cidr" {
+  type        = string
+  description = "CIDR block for the virtual network"
+}
 
+variable "public_subnet_cidr" {
+  type        = string
+  description = "CIDR block for the public subnet"
+}
+
+variable "private_subnet_cidr" {
+  type        = string
+  description = "CIDR block for the private subnet"
+}
+
+variable "allowed_ssh_ip" {
+  type        = string
+  description = "CIDR block allowed for SSH access"
+}
+
+variable "admin_username" {
+  type        = string
+  description = "Admin username for the virtual machine"
+}
 
 variable "instance_count" {
-  type = number
+  type        = number
+  description = "Initial number of VMSS instances"
 }
 
 variable "min_instance_count" {
-  type = number
+  type        = number
+  description = "Minimum number of VMSS instances for autoscaling"
 }
 
 variable "max_instance_count" {
-  type = number
+  type        = number
+  description = "Maximum number of VMSS instances for autoscaling"
 }
 
 variable "scale_out_cpu_threshold" {
-  type = number
+  type        = number
+  description = "CPU percentage threshold to trigger scale out"
 }
 
 variable "scale_in_cpu_threshold" {
-  type = number
+  type        = number
+  description = "CPU percentage threshold to trigger scale in"
 }
 
-variable "alert_email" {}
-
+variable "alert_email" {
+  type        = string
+  description = "Email address for monitoring alerts"
+}
